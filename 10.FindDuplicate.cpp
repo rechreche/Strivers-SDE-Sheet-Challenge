1372d2e6efa8e62b964124e0fcc38c980a3dd9e2
rechreche <135737371+rechreche@users.noreply.github.com>
2023-06-20 14:34:46 +0530

    10.FindDuplicate.cpp

---
+++ 10.FindDuplicate.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+	unordered_map<int,int> mp;
+
+	for(int i=0;i<n;i++)
+	{
+		mp[arr[i]]++;
+	}
+	for(auto el:mp)
+	{
+		if(el.second>1)
+		return el.first;
+	}
+}
