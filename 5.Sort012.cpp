d856d5872a4e3d4f1b1a5598e796500872846ded
rechreche <135737371+rechreche@users.noreply.github.com>
2023-06-20 14:25:08 +0530

    5.Sort012.cpp

---
+++ 5.Sort012.cpp
@@ -0,0 +1,27 @@
+//Using three pointer approach
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   int low=0,mid=0,high=n-1;
+
+   while(mid<=high)
+   {
+      if(arr[mid]==0)
+      {
+         swap(arr[low],arr[mid]);
+         low++;
+         mid++;
+      }
+      else if(arr[mid]==1)
+      mid++;
+
+      else if(arr[mid]==2)
+      {
+         swap(arr[mid],arr[high]);
+         high--;
+         //mid++;
+      }
+   }
+}
