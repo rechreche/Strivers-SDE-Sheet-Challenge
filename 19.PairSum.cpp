ba8ff06f00bef7156835b5ed1b9e88652fd73795
rechreche <135737371+rechreche@users.noreply.github.com>
2023-06-26 19:09:13 +0530

    19.PairSum.cpp

---
+++ 19.PairSum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   // Write your code here.
+   vector<vector<int>> ans;
+
+   sort(arr.begin(),arr.end());
+   for(int i=0;i<arr.size();i++)
+   {
+      for(int j=i+1;j<arr.size();j++)
+      {
+         if(arr[i]+arr[j] == s)
+         ans.push_back({arr[i],arr[j]});
+      }
+   }
+
+   return ans;
+
+
+}
