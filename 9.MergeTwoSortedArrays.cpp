bb4742554e6d4be21b980a36ae82c058bab5a6f3
rechreche <135737371+rechreche@users.noreply.github.com>
2023-06-27 16:25:46 +0530

    9.MergeTwoSortedArrays.cpp

---
+++ 9.MergeTwoSortedArrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	// Write your code here.
+
+	vector<int> ans(m+n);
+
+	int i=0,j=0,k=0;
+	while(i<m && j<n)
+	{
+		if(arr1[i] <=arr2[j])
+		{
+			ans[k++]=arr1[i++];
+		}
+		else
+		ans[k++]=arr2[j++];
+	}
+	while(i<m)
+	ans[k++]=arr1[i++];
+
+	while(j<n)
+	ans[k++]=arr2[j++];
+
+	return ans;
+}
