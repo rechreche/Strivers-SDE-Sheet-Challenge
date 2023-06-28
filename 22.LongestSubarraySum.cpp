bc0ab1910797382247506df532bbf2526da00aa7
rechreche <135737371+rechreche@users.noreply.github.com>
2023-06-28 19:21:48 +0530

    22.LongestSubarraySum.cpp

---
+++ 22.LongestSubarraySum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int LongestSubsetWithZeroSum(vector < int > arr) {
+
+  // Write your code here
+  int ans=0;
+  int sum=0;
+  unordered_map<int,int> mp;
+
+  for(int i=0;i<arr.size();i++)
+  {
+    sum+=arr[i];
+    if(sum==0)
+    ans=i+1;
+
+    
+    if(mp.find(sum) != mp.end())
+    {
+      ans=max(ans,i-mp[sum]);
+    }
+    else
+    mp[sum]=i;
+  }
+  return ans;
+
+}
